bf6729aa5a52b985d232c069f4937529986bd09b
Leon <lonphi12@gmail.com>
2022-02-24 11:21:10 -0500

    Added updatePlayerState() to track player states. Added Cinemachine package for camera

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,25 +10,37 @@ public class PlayerMovement : MonoBehaviour
     private float jumpForce = 30f;
     private float horizontalMovement;
     private float verticalMovement;
+    
+    // Currently used for debugging purposes
+    private enum characterState {idle, running, jumping, falling}
+    private TextMesh playerStateText;
+    private characterState playerState;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
         playerCollider = GetComponent<BoxCollider2D>();
+        playerStateText = GetComponentInChildren<TextMesh>();
     }
 
     void Update()
     {
         horizontalMovement = Input.GetAxisRaw("Horizontal");
         verticalMovement = Input.GetAxisRaw("Vertical");
+
+        updatePlayerState();
     }
 
     void FixedUpdate()
     {
         if(horizontalMovement > .1f || horizontalMovement < -.1f)
+        {
             rb2d.AddForce(new Vector2(horizontalMovement * moveSpeed, 0f), ForceMode2D.Impulse);
+        }
         if(verticalMovement > .1f && isGrounded())
+        {
             rb2d.AddForce(new Vector2(0f, verticalMovement * jumpForce), ForceMode2D.Impulse);
+        }
     }
 
     private bool isGrounded()
@@ -36,12 +48,37 @@ public class PlayerMovement : MonoBehaviour
         // Tests if a player is grounded by casting a ray and checking if the ray is colliding with any existing colliders.
         // 'Queries start in colliders' is disabled in Project Settings -> Physics2D in order for raycast to ignore its own collider.
 
-        float extraHeightTest = 0.02f;
+        float extraHeightTest = 0.015f;
 
         // Casts a ray from the center-bottom of the player's box collider
         RaycastHit2D rayCastTest = Physics2D.Raycast(playerCollider.bounds.center, Vector2.down, playerCollider.bounds.extents.y + extraHeightTest);
 
-        // Will return null if there is no collision between the ray and a collider.
-        return rayCastTest.collider != null;
+        return rayCastTest;
+    }
+
+    private void updatePlayerState()
+    {
+        if(horizontalMovement > .1f || horizontalMovement < -.1f)
+        {        
+            playerState = characterState.running;
+            playerStateText.text = "running";
+        }
+        else
+        {
+            playerState = characterState.idle;
+            playerStateText.text = "idle";
+        }
+        
+        if(verticalMovement > .1f)
+        {
+            playerState = characterState.jumping;
+            playerStateText.text = "jumping";
+        }
+        
+        if(rb2d.velocity.y < -.1f)
+        {
+            playerState = characterState.falling;
+            playerStateText.text = "falling";
+        }
     }
 }
