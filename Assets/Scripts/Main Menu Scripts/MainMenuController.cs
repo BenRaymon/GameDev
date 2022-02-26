b02463e693960c40a4a263bdf3cf3792ed04b941
Leon <lonphi12@gmail.com>
2022-02-26 15:13:35 -0500

    Added main menu and associated controller script. Organized Scripts folder. Imported TextMesh Pro.

---
+++ Assets/Scripts/Main Menu Scripts/MainMenuController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public string newGameLevel;
+
+    public void startGameNewGame()
+    {
+        SceneManager.LoadScene(newGameLevel);
+    }
+
+    public void startGameTestArea()
+    {
+        SceneManager.LoadScene(newGameLevel);
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
