2677ee6a3ce257e44c86e43a0ec347b6d5ee8133
PhiLE1 <94004194+PhiLE1@users.noreply.github.com>
2022-03-14 13:45:15 -0400

    tweaked some values in ImprovedMovement. Added documentation.

--- Assets/Scripts/Player Scripts/ImprovedMovement.cs
+++ Assets/Scripts/Player Scripts/ImprovedMovement.cs
@@ -16,13 +16,13 @@ public class ImprovedMovement : MonoBehaviour
 	private Animator playerAnimator;
 
 	// DATA CONSTANTS
-	[SerializeField] private PlayerData data;
+	[SerializeField] private PlayerData data; // gets data about the player from scriptableobject
 
 	// JUMP SETUP
 	private bool isChargedAttack = false;
 	private float chargeTimer = 0f;
-	private float coyoteTimer;
-	private float jumpBufferTimer;
+	private float coyoteTimer; // used for jump forgiveness after leaving the ground
+	private float jumpBufferTimer; // used for queueing a jump
 
 	// INPUT PARAMS (A/D and >/<)
 	private float horizontalMovement;
@@ -31,9 +31,9 @@ public class ImprovedMovement : MonoBehaviour
 	private enum characterState {idle, running, jumping, falling, chargingJump}
 	private characterState playerState;
 	private TextMesh playerStateText;
-	private bool isFacingRight = true;
-	private bool isJumping = false;
-	private bool queuedJump = false;
+	private bool isFacingRight = true; // used to detect facing direction
+	private bool isJumping = false; // used to move jumping to FixedUpdate()
+	private bool queuedJump = false; // used for queueing a jump
 
     void Awake()
 	{
@@ -45,23 +45,23 @@ public class ImprovedMovement : MonoBehaviour
 
 	void Start()
 	{
-		setGravityScale(data.GRAVITY);
+		setGravityScale(data.GRAVITY); // sets default gravity
 	}
 
 	void Update()
 	{
 		horizontalMovement = Input.GetAxisRaw("Horizontal");
 
-		checkFacingDirection(horizontalMovement > 0f);
+		checkFacingDirection(horizontalMovement > 0f); // checks facing direction based on input
 
-		checkForJump();
+		checkForJump(); // checks keys for jump while also handling coyote time and jump queueing
 
-		updatePlayerState();
+		updatePlayerState(); // state machine update
 
 		if (playerBody.velocity.y >= 0)
-            setGravityScale(data.GRAVITY);
+            setGravityScale(data.GRAVITY); // sets default gravity when jumping
 		else
-			setGravityScale(data.GRAVITY * data.FALL_GRAVITY_MULT);
+			setGravityScale(data.GRAVITY * data.FALL_GRAVITY_MULT); // sets higher gravity when falling
 	}
 
 	void FixedUpdate()
@@ -91,12 +91,15 @@ public class ImprovedMovement : MonoBehaviour
 
 	private void movePlayer()
 	{
-		float targetSpeed = horizontalMovement * data.MOVE_SPEED;
-		float speedDiff = targetSpeed - playerBody.velocity.x;
-		//Debug.Log(speedDiff);
+		float targetSpeed = horizontalMovement * data.MOVE_SPEED; // can be 0, -MOVE_SPEED, or +MOVE_SPEED
 
+		// takes the difference to find out how much force should be applied. Larger differences mean larger forces (such as when the player wants to turn)
+		float speedDiff = targetSpeed - playerBody.velocity.x;
+
+		// How fast the player should accelerate. Equivalent to how we accelerate when we start to run.
 		float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? data.RUN_ACCEL : data.RUN_DECELL;
 
+		// determines the curve at which the player speeds up. Used to make movements feel more natural.
 		float velocityPow;
 		if (Mathf.Abs(targetSpeed) < 0.01f)
 		{
@@ -111,6 +114,7 @@ public class ImprovedMovement : MonoBehaviour
 			velocityPow = data.ACCEL_POWER;
 		}
 
+		// force to be applied
 		float movement = Mathf.Pow(Mathf.Abs(speedDiff) * accelRate, velocityPow) * Mathf.Sign(speedDiff);
 
 		playerBody.AddForce(movement * Vector2.right);
@@ -118,7 +122,7 @@ public class ImprovedMovement : MonoBehaviour
 
 	private void checkForJump()
 	{
-		bool grounded = isGrounded();
+		bool grounded = isGrounded(); // ground check
 		jumpBufferTimer -= Time.deltaTime;
 
 		// Coyote time controller
@@ -142,6 +146,8 @@ public class ImprovedMovement : MonoBehaviour
 			jumpBufferTimer -= Time.deltaTime;
 		}
 
+		// used to separate queued jumping from regular jump. If a jump is queued and conditions are met,
+		// a jump should automatically be executed the next time the player is grounded.
 		if(queuedJump)
 		{
 			if(jumpBufferTimer > 0f && grounded)
@@ -217,6 +223,7 @@ public class ImprovedMovement : MonoBehaviour
 		playerBody.gravityScale = scale;
 	}
 
+	// determines direction to apply drag in and applies it.
 	private void Drag(float amount)
 	{
 		Vector2 force = amount * playerBody.velocity.normalized;
--- Assets/Scripts/Player Scripts/PlayerData.cs
+++ Assets/Scripts/Player Scripts/PlayerData.cs
@@ -30,4 +30,34 @@ public class PlayerData : ScriptableObject
 	[Header("Drag")]
 	public float AIR_DRAG;
 	public float GROUND_DRAG;
+
+	/*
+
+	NOTE: Git won't detect any modifications to the PlayerData.asset created in Scripts/Player Scripts/Data
+		CAUSE: Unknown
+
+	NOTE: Planning to add different accel/deccel while in the air later
+
+	DEFAULT VALUES FOR LEVEL01MERGE (They are supposed to be constants, but are currently set as public
+	so that they can be tweaked to improve the movement)
+	GROUND_BUFFER = .02
+	JUMP_FORCE = 20
+	JUMP_QUEUE = 0.5
+	COYOTE_TIME = 0.5
+
+	GRAVITY = 2
+	FALL_GRAVITY_MULT = 2
+
+	MOVE_SPEED = 20
+	RUN_ACCEL = 2
+	RUN_DECCEL = 3
+
+	STOP_POWER = 1.3
+	TURN_POWER = 1
+	ACCEL_POWER = 1.2
+
+	AIR_DRAG = 0.4
+	GROUND_DRAG = 0.4
+	*/
+
 }
