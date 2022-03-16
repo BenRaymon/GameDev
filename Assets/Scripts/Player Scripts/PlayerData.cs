62c7d3b924b8166b5ec6b701a351ba5a1ec19aa1
PhiLE1 <94004194+PhiLE1@users.noreply.github.com>
2022-03-16 14:40:47 -0400

    Fixed extension error. Added missing data value in PlayerData.

--- Assets/Scripts/Player Scripts/PlayerData.cs
+++ Assets/Scripts/Player Scripts/PlayerData.cs
@@ -18,16 +18,21 @@ public class PlayerData
 	public PlayerData(){
 		//DEFAULTS
 		GROUND_BUFFER = .02f;
+
 		JUMP_FORCE = 20f;
 		JUMP_QUEUE = 0.5f;
 		COYOTE_TIME = 0.5f;
+
 		GRAVITY = 2f;
 		FALL_GRAVITY_MULT = 2f;
+
 		MOVE_SPEED = 20f;
 		RUN_ACCEL = 2f;
+		RUN_DECELL = 3f;
 		STOP_POWER = 1.3f;
 		TURN_POWER = 1f;
 		ACCEL_POWER = 1.2f;
+		
 		AIR_DRAG = 0.4f;
 		GROUND_DRAG = 0.4f;
 	}
